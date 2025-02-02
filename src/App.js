d6b123b9e7c16222f7ee90684c263ed761930912
HNgoc <lmtcv1.9@gmail.com>
2025-02-02 20:12:09 +0700

    change backend url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [ower, setOwer] = useState(null);
   const socket = useRef(null);
   useEffect(() => {
-    socket.current = io('http://localhost:3001');
+    socket.current = io('https://caro-backend.onrender.com');
 
     socket.current.on(`find-player-wait`, () => {
       setFind(true);
