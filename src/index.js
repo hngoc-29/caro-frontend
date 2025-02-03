d1283d2330e6408a316302c82e4d73400dbecb2b
HNgoc <lmtcv1.9@gmail.com>
2025-02-03 14:18:35 +0700

    update metadata

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Helmet } from "react-helmet";
 import App from './App';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,16 +8,6 @@ import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Helmet>
-      <meta name="description" content="Một tựa game multiplayer cổ điển - Caro." />
-      <meta name="keywords" content="Caro, Caro online, multiplayer, Nhiều người chơi" />
-      <meta name="robots" content="index, nofollow" />
-      <meta name="author" content="h_ngoc" />
-      <meta property="og:title" content="Caro Online" />
-      <meta property="og:description" content="Một tựa game multiplayer cổ điển - Caro." />
-      <meta property="og:image" content="/caro.png" />
-      <meta property="og:type" content="game" />
-    </Helmet>
     <App />
     <ToastContainer />
   </>
